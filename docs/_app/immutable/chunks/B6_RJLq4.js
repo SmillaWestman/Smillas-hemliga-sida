38611ebaeb717ae85ad125eb9e9f20373b70b8de
Smilla Westman <smives@edu.umea.se>
2025-11-13 10:35:39 +0100

    message

---
+++ docs/_app/immutable/chunks/B6_RJLq4.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_128a0hf?.base??"",a=globalThis.__sveltekit_128a0hf?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/DjaxKOR7.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1iuuodt?.base??"",t=globalThis.__sveltekit_1iuuodt?.assets??s??"";export{t as a,s as b};
