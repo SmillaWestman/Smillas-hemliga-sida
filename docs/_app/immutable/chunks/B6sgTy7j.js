250cbd0f3bdd986c4c766aa5e03110bf120eebf2
Smilla Westman <smives@edu.umea.se>
2026-01-29 10:24:51 +0100

    message

---
+++ docs/_app/immutable/chunks/B6sgTy7j.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_7adzn?.base??"/pokemon-search",a=globalThis.__sveltekit_7adzn?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/BNOefwhj.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_4cf50b?.base??"",a=globalThis.__sveltekit_4cf50b?.assets??s??"";export{a,s as b};
